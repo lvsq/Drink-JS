6b9e6bdd303623df531c0c33476f1561b8506d6b
lvsq <austin.lvsq@gmail.com>
2017-04-12 12:48:35 -0400

    Over all Directory changes.Ripped out ruby as the deployment tool. Ripped out some useless directories. changed some things in package.json. changed some commands to use the non deprecated commands. added somemore  logging. commented out the mongo stuff. working on a replacement.

--- config/deploy/dev.rb
+++
@@ -1,4 +0,0 @@
-server "purpledrank.csh.rit.edu", :app, :web, :db, :primary => true
-set :deploy_to, "/home/drink/drink_dev"
-
-after "deploy", "develop:deps", "develop:configs", "develop:stop", "develop:start"
